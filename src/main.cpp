b4403e98fe32deec0999cefe5989516a1188b655
ayan4m1 <andrew@bulletlogic.com>
2026-05-01 12:15:12 -0400

    feat: initial implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,105 +1,140 @@
-#include <esp_http_client.h>
-#include <esp_netif_sntp.h>
-#include <esp_sntp.h>
-#include <esp_wifi.h>
+#include <esp_sleep.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
-#include <htcw_json.h>
 
-#include "custom_panel.h"
-#include "gfx.hpp"
-#include "panel.h"
-#include "uix.hpp"
+#include "M5GFX.h"
+#include "M5PM1.h"
+#include "M5Unified.h"
 
-#define TELEGRAMA_IMPLEMENTATION
-#include "telegrama.hpp"
-#undef TELEGRAMA_IMPLEMENTATION
+#define POMODORO_INTERVAL_MINS 1
 
-using namespace gfx;
-using namespace uix;
-using namespace json;
-
-static uix::display lcd;
+static M5PM1 power;
 
 // snprintf format strings
 static const char* time_format_string = " %02d:%02d";
-static const char* ready_format_string = "Ready";
-static const char* done_format_string = "Done";
 
-// to hold strings formatted for display
-static char time_text[15] = {0};
-
-void panel_lcd_flush_complete(void) {
-  // let UIX know the DMA transfer completed
-  lcd.flush_complete();
-}
-static void uix_flush(const rect16& bounds, const void* bitmap, void* state) {
-  // similar to LVGL
-  panel_lcd_flush(bounds.x1, bounds.y1, bounds.x2, bounds.y2, (void*)bitmap);
-}
-
-// type to represent our display
-using screen_t = uix::screen<gsc_pixel<LCD_BIT_DEPTH>>;
-// native screen color
-using scr_color_t = color<screen_t::pixel_type>;
-// UIX color (rgba32)
-using uix_color_t = color<uix_pixel>;
-
-// fonts and screens
-static tt_font clock_text_font(telegrama, 20, font_size_units::px);
-
-static screen_t main_screen;
-
-// control types
-using label_t = uix::label<screen_t::control_surface_type>;
-using icon_t = uix::image_box<screen_t::control_surface_type>;
-using vlabel_t = uix::vlabel<screen_t::control_surface_type>;
-using rect_t = uix::painter<screen_t::control_surface_type>;
-
-label_t time_label;
+uint32_t last_active = 0;
+uint32_t previous_millis = 0;
+uint32_t last_blink = 0;
+uint8_t seconds = 0;
+uint8_t minutes = POMODORO_INTERVAL_MINS;
+bool backlight_enabled = true;
+bool timer_running = true;
+bool timer_elapsed = false;
+bool blink_active = false;
+bool clear_display = true;
 
 void draw_screen() {
-  snprintf(time_text, sizeof(time_text), time_format_string, 6, 42);
+  uint8_t battery_pct = M5.Power.getBatteryLevel();
 
-  time_label.text(time_text);
+  if (backlight_enabled || timer_elapsed) {
+    M5.Lcd.setBrightness(40);
+  } else {
+    M5.Lcd.setBrightness(0);
+  }
 
-  while (lcd.dirty()) {
-    lcd.update();
+  if (timer_elapsed) {
+    M5.Lcd.clearDisplay(TFT_RED);
+
+    uint32_t current_millis = pdTICKS_TO_MS(xTaskGetTickCount());
+
+    if (current_millis - last_blink > 1500) {
+      blink_active = !blink_active;
+    }
+
+    if (blink_active) {
+      M5.Lcd.setTextColor(TFT_WHITE);
+      M5.Lcd.setCursor(58, 67 - 14);
+      M5.Lcd.print("00:00");
+    }
+  } else {
+    M5.Lcd.setTextSize(2);
+    M5.Lcd.setTextColor(TFT_WHITE);
+    M5.Lcd.fillRect(2, 2, 62, 20, TFT_BLACK);
+    M5.Lcd.setCursor(2, 2);
+    M5.Lcd.printf("%d%%", battery_pct);
+
+    M5.Lcd.setTextSize(4);
+    M5.Lcd.setTextColor(TFT_ORANGE);
+    M5.Lcd.fillRect(34, 67 - 14, 240 - 32, 135 - 67 - 14, TFT_BLACK);
+    M5.Lcd.setCursor(34, 67 - 14);
+    M5.Lcd.printf(time_format_string, minutes, seconds);
+  }
+}
+
+void toggle_timer() { timer_running = !timer_running; }
+
+void reset_timer() {
+  minutes = POMODORO_INTERVAL_MINS;
+  seconds = 0;
+  timer_running = false;
+  timer_elapsed = false;
+  M5.Lcd.clearDisplay(TFT_BLACK);
+}
+
+void tick_timer() {
+  if (seconds-- == 0) {
+    seconds = 59;
+    if (minutes-- == 0) {
+      minutes = POMODORO_INTERVAL_MINS;
+      seconds = 0;
+      timer_running = false;
+      timer_elapsed = true;
+      last_blink = pdTICKS_TO_MS(xTaskGetTickCount());
+    }
   }
 }
 
 void app_loop(void* params) {
+  last_active = pdTICKS_TO_MS(xTaskGetTickCount());
+
   while (true) {
-    uint32_t started = pdTICKS_TO_MS(xTaskGetTickCount());
+    M5.update();
+    bool btnA = M5.BtnA.wasPressed();
+    bool btnB = M5.BtnB.wasPressed();
 
+    if (btnA) {
+      toggle_timer();
+    }
+
+    if (btnB) {
+      reset_timer();
+    }
+
+    if (btnA || btnB) {
+      last_active = pdTICKS_TO_MS(xTaskGetTickCount());
+    }
+
+    if (timer_running) {
+      uint32_t current_millis = pdTICKS_TO_MS(xTaskGetTickCount());
+
+      if (current_millis - previous_millis >= 1000) {
+        previous_millis = current_millis;
+        tick_timer();
+      }
+    }
+
+    backlight_enabled =
+        (pdTICKS_TO_MS(xTaskGetTickCount()) - last_active <= 10000) ||
+        timer_elapsed;
+
+    vTaskDelay(pdMS_TO_TICKS(50));
+  }
+}
+
+void drawing_loop(void* arg) {
+  while (true) {
     draw_screen();
-
-    uint16_t elapsed = pdTICKS_TO_MS(xTaskGetTickCount()) - started;
-
-    vTaskDelay(pdMS_TO_TICKS(1000 - elapsed));
+    vTaskDelay(pdMS_TO_TICKS(800));
   }
 }
 
 extern "C" void app_main() {
-  panel_lcd_init();
-
-  lcd.buffer_size(LCD_TRANSFER_SIZE);
-  lcd.buffer1((uint8_t*)panel_lcd_transfer_buffer());
-  lcd.buffer2((uint8_t*)panel_lcd_transfer_buffer2());
-  lcd.on_flush_callback(uix_flush);
-
-  clock_text_font.initialize();
-
-  main_screen.dimensions({LCD_WIDTH, LCD_HEIGHT});
-  main_screen.background_color(scr_color_t::black);
-
-  time_label.bounds(srect16(0, 0, LCD_WIDTH - 1, LCD_HEIGHT - 1));
-  time_label.font(clock_text_font);
-  time_label.color(uix_color_t::purple);
-  time_label.text_justify(uix_justify::center);
-  main_screen.register_control(time_label);
-
-  lcd.active_screen(main_screen);
+  power.begin();
+  M5.begin();
+  M5.Lcd.setRotation(1);
+  M5.Lcd.setBrightness(40);
 
   xTaskCreatePinnedToCore(app_loop, "app", 8192, nullptr, 10, nullptr, 0);
+  xTaskCreatePinnedToCore(drawing_loop, "draw", 8192, nullptr, 10, nullptr, 1);
 }
